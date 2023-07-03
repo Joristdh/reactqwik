c7968192f3d6811b86b73e6527b3e181008c9734
Joris <5628733-Joristdh@users.noreply.gitlab.com>
2023-07-03 16:43:23 +0200

    Simplify

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,45 +2,34 @@ import { component$, useSignal, useStyles$ } from "@builder.io/qwik";
 
 export default component$(() => {
     useStyles$(/* css */`
-        main {
-            height: 100vh;
-            overflow: hidden;
-        }
-
-        h1 {
-            display: none;
-            user-select: none;
-
-            &.display {
-                display: block;§
-            }
-        }
-
         span {
-            display: none;
             font-size: 25em;
             height: 100vh;
             line-height: 75vh;
-
-            &.display {
-                display: inline-block;
-            }
         }
     `);
 
-    const vis = useSignal(0);
+    const vis = useSignal(1);
 
     return <>
-        <main class="cursor-pointer" onClick$={() => {
-            vis.value = 4;
-            setTimeout(() => {
-                vis.value = Math.floor(Math.random() * 2) + 1
-            }, (Math.floor(Math.random() * 3) + 3) * 1000)
-        }}>
+        <main class={["cursor-pointer select-none overflow-hidden h-screen",
+            { "pointer-events-none": vis.value === 0 }]} onClick$={() => {
+                vis.value = 0;
+                setTimeout(() => {
+                    vis.value = Math.floor(Math.random() * 2) + 2
+                }, (Math.floor(Math.random() * 3) + 3) * 1000)
+            }}>
             <div class="flex justify-center items-center h-screen">
-                <h1 class={{ "display": vis.value === 0 }}>Press anywhere to start</h1>
-                <span class={{ "display": vis.value === 1 }}>&#8680;</span>
-                <span class={{ "display": vis.value === 2 }}>&#8678;</span>
+                {(() => {
+                    switch (vis.value) {
+                        case 1:
+                            return <h1>Press anywhere to start</h1>
+                        case 2:
+                            return <span>&#8680;</span>
+                        case 3:
+                            return <span>&#8678;</span>
+                    }
+                })()}
             </div>
         </main>
     </>
